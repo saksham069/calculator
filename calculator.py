b53dcd42483e974542617c01d75430a8290b7256
Saksham Arora <sakshamarorax02@gmail.com>
2023-11-18 10:33:02 +0530

    multiplication and division

--- calculator.py
+++ calculator.py
@@ -1,6 +1,12 @@
 def addition(n1, n2):
     return n1 + n2
 
+def multiplication(n1, n2):
+    return n1*n2
+
+def division(n1, n2):
+    return n1/n2
+
 def main():
     num1 = int(input("first nunber: "))
     num2 = int(input("second nunber: "))
@@ -8,6 +14,10 @@ def main():
 
     if op == '+':
         print(addition(num1, num2))
+    elif op == '*':
+        print(multiplication(num1, num2))
+    elif op == '/':
+        print(division(num1, num2))
     else:
         print("Invalid!")
 
